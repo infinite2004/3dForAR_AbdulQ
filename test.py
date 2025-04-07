d8609dd16972808b8435cda650531cb56cb926f3
infinite2004 <qureshi.abdul.227@gmail.com>
2025-04-07 08:52:19 -0400

    remove some stuff

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ from PIL import Image, ImageOps
 
 # === Spotify Setup ===
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="4f0c60d1b7254d27b6c1b2129234b962",
-    client_secret="d7109c33f6c446bfba529c04e68eb908",
+    client_id="",
+    client_secret="",
     redirect_uri="http://localhost:8888/callback/",
     scope="user-read-currently-playing"
 ))
