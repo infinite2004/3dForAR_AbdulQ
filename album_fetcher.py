65b2cfa07cd243d4c878d3902681a71e2394b6e4
Abdul Qureshi <44120607+infinite2004@users.noreply.github.com>
2025-04-15 19:58:35 -0400

    Update  album_fetcher.py

--- album_fetcher.py
+++ album_fetcher.py
@@ -6,8 +6,8 @@ from io import BytesIO
 import time
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="4f0c60d1b7254d27b6c1b2129234b962",
-    client_secret="d7109c33f6c446bfba529c04e68eb908",
+    client_id="",
+    client_secret="",
     redirect_uri="http://127.0.0.1:8888/callback",
     scope="user-read-currently-playing"
 ))
